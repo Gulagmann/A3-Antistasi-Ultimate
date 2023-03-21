497719f709f9045a3b2af239adfbd2d5a7f36eed
Sebastian <s.schmidt-jensen@outlook.de>
2023-03-21 20:17:35 +0100

    Added CISEF Vehicles & Loot Crate Logistics

---
+++ A3A/addons/logistics/Cargo/CISEF.hpp
@@ -0,0 +1,15 @@
+//class cisef_vehicles_turret_m2_CISEF_Trt_M2_high_Pacific_p3d : TRIPLES(ADDON,Cargo,Base)
+//{
+//    offset[] = {0.2, -0.5, 1.69};
+//    rotation[] = {0, 1, 0};
+//    size = 4;
+//    recoil = 100;
+//    isWeapon = 1;
+//}
+
+class cisef_structures_box_CISEF_Ruggedbox : TRIPLES(ADDON,Cargo,Base)
+{
+    offset[] = {0,0,0.1};
+    rotation[] = {0,0,0};
+    size = 1;
+}
---
+++ A3A/addons/logistics/Nodes/CISEF.hpp
@@ -0,0 +1,58 @@
+class cisef_vehicles_wheeled_offroad_offroad_01_cisef_f_p3d : TRIPLES(ADDON,Nodes,Base)
+{
+    class Nodes
+    {
+        class Node1
+        {
+            offset[] = {-0.05,-0.9,-0.683};
+            seats[] = {3,4};
+        }
+        class Node2
+        {
+            offset[] = {-0.05,-1.6,-0.683};
+             seats[] = {};
+        }
+    }
+}
+
+class cisef_vehicles_wheeled_typhoon_typhoon_flatbed_8x8_p3d : TRIPLES(ADDON,Nodes,Base)
+{
+    class Nodes
+    {
+        class Node1
+        {
+            offset[] = {0,0.7,-0.8};
+            seats[] = {};
+        }
+        class Node2
+        {
+            offset[] = {0,-0.1,-0.8};
+            seats[] = {};
+        }
+        class Node3
+        {
+             offset[] = {0,-0.9,-0.8};
+             seats[] = {};
+        }
+        class Node4
+        {
+              offset[] = {0,-1.7,-0.8};
+              seats[] = {};
+        }
+        class Node5
+        {
+               offset[] = {0,-2.5,-0.8};
+               seats[] = {};
+        }
+        class Node6
+        {
+               offset[] = {0,-3.3,-0.8};
+               seats[] = {};
+        }
+        class Node7
+        {
+               offset[] = {0,-4.1,-0.8};
+               seats[] = {};
+        }
+    }
+}
