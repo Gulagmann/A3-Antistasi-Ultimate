15cab8cb498617028d38f9ed807ab0b6ceb522e0
Sebaa <s.schmidt-jensen@outlook.de>
2023-04-06 23:10:03 +0200

    Buffed Loot Box Armor so it doesn't randomly discombobulate

--- A3A/addons/core/CfgVehicles.hpp
+++ A3A/addons/core/CfgVehicles.hpp
@@ -1,8 +1,14 @@
 class CfgVehicles
 {
     class Box_Syndicate_Ammo_F;
+    class CISEF_Box_Rugged_Loot_Olive;
 
     class A3AP_Box_Syndicate_Ammo_F : Box_Syndicate_Ammo_F 
+    {
+        armor = 2000;
+    };
+
+    class A3AP_CISEF_Box_Rugged_Loot_Olive : CISEF_Box_Rugged_Loot_Olive
     {
         armor = 2000;
     };
